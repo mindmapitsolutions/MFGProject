ef8456b66a9bd09eca2245325ede8d9f441282bc
Ashnil Pawar <ashnil@LAPTOP-S2S7M7G8>
2017-02-09 10:28:22 +0530

    Test Checkin

--- scripts/dashboard/dashboard1/_dashboard1.js
+++ scripts/dashboard/dashboard1/_dashboard1.js
@@ -3,7 +3,6 @@
     angular.module("dashboard.dashboard1", [
         //"dashboard.dashboard1",
         //"dashboard.dashboard2",
-
         //"dashboard.dashboard3",
         //"dashboard.dashboard4",
     ]);
